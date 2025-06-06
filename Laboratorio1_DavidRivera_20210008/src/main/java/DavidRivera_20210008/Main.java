afe2658418edf49381646a8cdabdf8fe353ab9b4
david <20210008>
2025-06-06 11:24:10 -0600

    nuevos cambios

--- Laboratorio1_DavidRivera_20210008/src/main/java/DavidRivera_20210008/Main.java
+++ Laboratorio1_DavidRivera_20210008/src/main/java/DavidRivera_20210008/Main.java
@@ -7,7 +7,7 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        LinkedList<Productos> productos;
+        Productos productos;
         int opcion;
 
 
@@ -15,42 +15,39 @@ public class Main {
             System.out.println("Menu");
             System.out.println("1.Diagrama");
             System.out.println("2.Agregar Producto");
-            System.out.println("3.Mostrar Productos");
-            System.out.println("4.Actualizar Producto");
-            System.out.println("5.Eliminar Producto");
+            System.out.println("3.Actualizar Producto");
+            System.out.println("4.Eliminar Producto");
             System.out.println("0.Salir");
             opcion = sc.nextInt();
             sc.nextLine();
 
             switch (opcion){
                case 1->{
-                   System.out.println("MOstrando diagrama");
+                   System.out.println("Mostrando diagrama");
+                   productos.MostrarListado();
                    break;
                }
                case 2->{
-                   System.out.printf("Agregando el producto");
+                   System.out.println("Agregando el producto");
+                   productos.AgregarProducto();
+
                    break;
                }
+
                case 3->{
-                   System.out.println("Mostrando el listado de los productos");
-                   break;
-               }
-               case 4->{
                    System.out.println("Actualizando el producto");
                    break;
                }
-               case 5->{
+               case 4->{
                    System.out.println("Eliminando el producto");
+                   productos.EliminarProducto();
                    break;
                }
-               case 0->{
-                   System.out.println("Saliendo del sistema");
-                   
-               }
+
                 default -> {
                     System.out.println("elija una opcion ");
                 }
             }
-        }
+        }while (opcion != 0);
     }
 }
--- Laboratorio1_DavidRivera_20210008/src/main/java/DavidRivera_20210008/Productos.java
+++ Laboratorio1_DavidRivera_20210008/src/main/java/DavidRivera_20210008/Productos.java
@@ -6,11 +6,14 @@ public class Productos {
     private String id;
     private final String nombre;
     private final Double precio;
+    LinkedList<Productos> productos;
+
 
     public Productos(String id, String nombre,Double precio){
         this.id = id;
         this.nombre = nombre;
         this.precio = precio;
+        this.productos = new LinkedList<>();
     }
 
     public String getId() {
@@ -25,15 +28,20 @@ public class Productos {
         return precio;
     }
 
+    public LinkedList<Productos> getProductos() {
+        return productos;
+    }
 
     public void AgregarProducto(){
+        System.out.println("Ingrese el nuevo producto " + id + "con su nombre " + nombre + "del precio $ " + precio);
 
     }
 
-    public void MostrarProducto(){
-        LinkedList<String> Lista = new LinkedList<>();
+    public void MostrarListado(){
+        LinkedList<String>lista = new LinkedList<>();
 
-
+        System.out.println("Primer prducto " + lista.getFirst());
+        System.out.println("Ultimo producto "+ lista.getLast());
     }
 
     public void ActualizarProducto(){
@@ -41,6 +49,8 @@ public class Productos {
     }
 
     public void EliminarProducto(){
+        LinkedList<String>lista = new LinkedList<>();
 
+        lista.removeFirst();
     }
 }
